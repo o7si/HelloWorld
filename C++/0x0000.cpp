23878b8eff6ea47cbc1be4b81df273835337918c
shayuanshayuan <32285332+shayuanshayuan@users.noreply.github.com>
2019-04-01 15:06:58 +0800

    Update 0x0000.cpp

--- C++/0x0000.cpp
+++ C++/0x0000.cpp
@@ -2,6 +2,6 @@
 
 int main(int argc, char *argv[])
 {
-	std::cout << 0x0000 << " -> HelloWorld" << std::endl;
-	return 0;
+    std::cout << 0x0000 << " -> HelloWorld" << std::endl;
+    return 0;
 }
