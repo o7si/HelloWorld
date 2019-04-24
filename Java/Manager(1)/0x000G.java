f5c098ff3e94cdfd472af226b13fadf678fa262b
shayuanshayuan <32285332+shayuanshayuan@users.noreply.github.com>
2019-04-24 21:28:22 +0800

    Update 0x000G.java

--- Java/Manager(1)/0x000G.java
+++ Java/Manager(1)/0x000G.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println(0x000G + " -> HelloWorld");
+        System.out.println(0x0010 + " -> HelloWorld");
     }
 }
